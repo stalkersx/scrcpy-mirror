20d289c87db4a12ebb9d7ad37c8a35fe0f3dbb3f
stalkersx <stalkersx16@gmail.com>
2025-04-03 07:39:23 +0000

    # version 1.0

---
+++ Debian-sc.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo apt install adb wget cmake libavformat-dev pkg-config libsdl2-dev libavdevice-dev libusb-1.0-0-dev meson
+
+if [[ $? -eq 0 ]];then
+    if [ "scrcpy-mirror" ];then
+        dpkg -b scrcpy-mirror
+        sudo dpkg -i scrcpy-mirror.deb
+    else
+        echo "source dir [scrcpy] not found"
+    fi
+else
+    echo "! Depends not found on your distribution"
+fi
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # scrcpy-mirror
+# version 1.0
+
+INSTALL DEPENDS :
+-adb -wget -cmake -libavformat-dev -pkg-config
+-libsdl2-dev -libavdevice-dev -libusb-1.0-0-dev -meson
+- $ chmod +x Debian-sc.sh
+- $ ./Debian-sc.sh
+
+CREATE PACKAGE FILE :
+- $ dpkg -b scrcpy-mirror
+- $ sudo dpkg -i scrcpy-mirror.deb
+
+USAGE :
+- $ scrcpy --help
+  >> for information help
